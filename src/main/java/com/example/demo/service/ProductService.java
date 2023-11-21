0d37bc13ca6a07b07d03cde82e0f6e2c9f258560
Deepakmishra6372 <140227851+Deepakmishra6372@users.noreply.github.com>
2023-11-21 21:01:51 +0530

    adding service and utility package
    commit

---
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -0,0 +1,15 @@
+package com.example.demo.service;
+
+
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.demo.model.Product;
+
+
+
+public interface ProductService {
+	
+	void addProduct(Product product, MultipartFile productImmage);
+
+}
---
+++ src/main/java/com/example/demo/service/ProductServiceImpl.java
@@ -0,0 +1,34 @@
+package com.example.demo.service;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.demo.dao.ProductDao;
+import com.example.demo.model.Product;
+
+import com.example.demo.utility.StorageService;
+
+
+@Service
+public class ProductServiceImpl implements ProductService {
+	
+	@Autowired 
+	private ProductDao productDao;
+	
+	@Autowired
+	private StorageService storageService;
+
+	@Override
+	public void addProduct(Product product, MultipartFile productImmage) {
+		
+		String productImageName = storageService.store(productImmage);
+		
+		product.setImageName(productImageName);
+		
+		this.productDao.save(product);
+	}
+
+}
