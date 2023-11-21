9cbce9c80a02a94d34c54c1ef8cf6c96acb2f2e0
Subhasandhya <131661181+Subhasandhya@users.noreply.github.com>
2023-11-21 17:47:34 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/StoreManagement2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreManagement2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreManagement2Application.class, args);
+	}
+
+}
